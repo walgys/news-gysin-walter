f95a48c47fa6dc8e566e935f6f632dd5d97bdb26
Walter A Gysin <gysinwalter@gmail.com>
2020-09-30 09:28:29 -0300

    renamed components entry point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
