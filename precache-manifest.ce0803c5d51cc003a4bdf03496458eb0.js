84650a11b347fae0cc936c9583ebfd5f6fa082e4
Walter A Gysin <gysinwalter@gmail.com>
2020-11-24 08:42:50 -0300

    Updates

---
+++ precache-manifest.ce0803c5d51cc003a4bdf03496458eb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e561fe3070f36fd31bb1e944afce2079",
+    "url": "/news-gysin-walter/index.html"
+  },
+  {
+    "revision": "210d970dea2b3b05375d",
+    "url": "/news-gysin-walter/static/css/main.31abc597.chunk.css"
+  },
+  {
+    "revision": "fd3e1132bd8b5ca4bf46",
+    "url": "/news-gysin-walter/static/js/2.2f2ecadf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news-gysin-walter/static/js/2.2f2ecadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210d970dea2b3b05375d",
+    "url": "/news-gysin-walter/static/js/main.a7cbcf5c.chunk.js"
+  },
+  {
+    "revision": "b54847dda7d36af751cf",
+    "url": "/news-gysin-walter/static/js/runtime-main.4f005c89.js"
+  },
+  {
+    "revision": "50f877d9a3a5b5429523148e85a91855",
+    "url": "/news-gysin-walter/static/media/megaphone.50f877d9.svg"
+  }
+]);
